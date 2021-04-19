9b69bca355c42fd7ff3da09d590f168f7b5835d8
syldysnya <syldysnya@gmail.com>
2021-04-19 14:30:23 -0400

    Add changes to curried

--- arguments/curried_sum.js
+++ arguments/curried_sum.js
@@ -10,13 +10,22 @@ function curriedSum(num1) {
   }
 }
 
-Function.prototype.curry = function() {
-  return function sumTwo(num2){
-    
+Function.prototype.curry = function(limit) {
+  let nums = [];
+  let that = this.call(this);
+  // debugger
+  return function sumArr(num){
+    nums.push(num);
+    // debugger
+    if (nums.length === limit) {
+      that(...nums);
+    } else {
+      return sumArr;
+    }
   }
 }
 
-sumThree(4, 20, 6); // == 30
+// sumThree(4, 20, 6); // == 30
 
 // you'll write `Function#curry`!
 let f1 = sumThree.curry(3); // tells `f1` to wait until 3 arguments are given before running `sumThree`
@@ -28,5 +37,5 @@ f1 = f1(6); // = 30
 sumThree.curry(3)(4)(20)(6); // == 30
 
 
-const sum = curriedSum(4);
-sum(5)(30)(20)(1); // => 56
+// const sum = curriedSum(4);
+// sum(5)(30)(20)(1); // => 56
