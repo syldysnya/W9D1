bcc3eee0c941454397eed5904d26a1b321a056f5
Daniel Cho <thedanielcho@gmail.com>
2021-04-19 14:51:09 -0400

    curried sums

--- arguments/curried_sum.js
+++ arguments/curried_sum.js
@@ -1,41 +1,54 @@
-function sumThree(num1, num2, num3) {
-  return num1 + num2 + num3;
-}
+// function sumThree(num1, num2, num3) {
+//   return num1 + num2 + num3;
+// }
 
-function curriedSum(num1) {
-  return function addSumTwo(num2) {
-    return function addSumThree(num3) {
-      return num1 + num2 + num3;
-    }
-  }
-}
 
-Function.prototype.curry = function(limit) {
-  let nums = [];
-  let that = this.call(this);
-  // debugger
-  return function sumArr(num){
-    nums.push(num);
-    // debugger
-    if (nums.length === limit) {
-      that(...nums);
-    } else {
-      return sumArr;
-    }
-  }
-}
+
+// Function.prototype.curry = function(limit) {
+//   let nums = [];
+//   let that = this.bind(this);
+//   // debugger
+//   return function sumArr(num){
+//     nums.push(num);
+//     // debugger
+//     if (nums.length === limit) {
+//       return that(...nums);
+//     } else {
+//       return sumArr;
+//     }
+//   }
+// }
 
 // sumThree(4, 20, 6); // == 30
 
-// you'll write `Function#curry`!
-let f1 = sumThree.curry(3); // tells `f1` to wait until 3 arguments are given before running `sumThree`
-f1 = f1(4); // [Function]
-f1 = f1(20); // [Function]
-f1 = f1(6); // = 30
+// // you'll write `Function#curry`!
+// let f1 = sumThree.curry(3); // tells `f1` to wait until 3 arguments are given before running `sumThree`
+// f1 = f1(4); // [Function]
+// f1 = f1(20); // [Function]
+// f1 = f1(6); // = 30
 
-// or more briefly:
-sumThree.curry(3)(4)(20)(6); // == 30
+// // or more briefly:
+// sumThree.curry(3)(4)(20)(6); // == 30
 
 
-// const sum = curriedSum(4);
-// sum(5)(30)(20)(1); // => 56
+function curriedSum(numArgs) {
+  let nums = [];
+  return function _curriedSum(num){
+    debugger
+    nums.push(num);
+    if (nums.length === numArgs) {
+      let addNums = 0;
+      nums.forEach(function(ele){
+        debugger
+        addNums += ele;
+      })
+      return addNums
+    } else {
+      debugger
+      return _curriedSum;
+    }
+  }
+}
+
+const sum = curriedSum(4);
+sum(5)(30)(20)(1); // => 56
